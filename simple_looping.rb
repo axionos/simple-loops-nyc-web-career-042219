c35831407b9ac1cec31316daa5d6514fc3bf3926
SOYEONG OH <axionoso@gmail.com>
2019-03-18 22:33:14 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -34,10 +34,10 @@ def while_iterator(number_of_times)
   # hint: user a counter to tell the while loop when to stop!
   counter = 0
   while counter <= number_of_times
-
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
     counter += 1
   end
-  phrase = "Welcome to Flatiron School's Web Development Course!"
 
 end
 
